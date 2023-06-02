ba039198f7d71fcd21a63ad2e187fd7e7dc3dc8f
jidoggs <jidoggs@gmail.com>
2023-06-02 10:58:29 +0100

    upgraded react to v18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { render } from 'react-dom'
-import App from './App'
-import { ThemeProvider } from './contexts/theme'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "./contexts/theme";
+import "./index.css";
 
-render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <ThemeProvider>
     <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-)
+  </ThemeProvider>
+);
