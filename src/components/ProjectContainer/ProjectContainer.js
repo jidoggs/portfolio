350cbc9745b22c62c79479b82a79104cbd9edb73
jidoggs <jidoggs@gmail.com>
2023-06-02 02:23:39 +0100

    update

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -1,17 +1,18 @@
-import uniqid from 'uniqid'
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LaunchIcon from '@material-ui/icons/Launch'
-import './ProjectContainer.css'
+import uniqid from "uniqid";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import ArchitectureIcon from "@mui/icons-material/Architecture";
+import LaunchIcon from "@mui/icons-material/Launch";
+import "./ProjectContainer.css";
 
 const ProjectContainer = ({ project }) => (
-  <div className='project'>
+  <div className="project">
     <h3>{project.name}</h3>
 
-    <p className='project__description'>{project.description}</p>
+    <p className="project__description">{project.description}</p>
     {project.stack && (
-      <ul className='project__stack'>
+      <ul className="project__stack">
         {project.stack.map((item) => (
-          <li key={uniqid()} className='project__stack-item'>
+          <li key={uniqid()} className="project__stack-item">
             {item}
           </li>
         ))}
@@ -21,25 +22,35 @@ const ProjectContainer = ({ project }) => (
     {project.sourceCode && (
       <a
         href={project.sourceCode}
-        aria-label='source code'
-        className='link link--icon'
+        aria-label="source code"
+        className="link link--icon"
         target="_blank"
       >
         <GitHubIcon />
+      </a>
+    )}
+    {project.designLink && (
+      <a
+        href={project.designLink}
+        aria-label="design link"
+        className="link link--icon"
+        target="_blank"
+      >
+        <ArchitectureIcon />
       </a>
     )}
 
     {project.livePreview && (
       <a
         href={project.livePreview}
-        aria-label='live preview'
-        className='link link--icon'
+        aria-label="live preview"
+        className="link link--icon"
         target="_blank"
       >
         <LaunchIcon />
       </a>
     )}
   </div>
-)
+);
 
-export default ProjectContainer
+export default ProjectContainer;
--- src/portfolio.js
+++ src/portfolio.js
@@ -24,38 +24,25 @@ const projects = [
       "An ecommerce store where user inputs are helps in shaping the look of the product the user is trying to purchase. Built with React and three.js",
     stack: ["SASS", "JavaScript", "React", "Three.js"],
     sourceCode: "https://github.com/jidoggs/sneaker-city",
+    designLink:
+      "https://www.figma.com/file/9KCCJCcsP85071jFHSZvxJ/Sneaker-City%3A-A-concept-for-a-foot-wear-store-(Community)?type=design&t=bMFq10irql9lFuCW-1",
     livePreview: "http://sneaker-city.vercel.app",
   },
   {
-    name: "Traveler",
+    name: "Confam Money",
     description:
-      "A Travel application where a user can search for travel destination, get weather report of the said destination, travel advice about the said destinantion and others.",
-    stack: ["SASS", "JavaScript", "React"],
-    sourceCode: "https://github.com/jidoggs/traveler",
-    livePreview: "https://traveler-env-jidoggs.vercel.app/",
+      "Confam Money is a secure and reliable third party that holds and regualates payments of funds required for both buyers and seller involved in a given transaction",
+    stack: ["SASS", "Tailwind", "TypeScript", "React"],
+    designLink:
+      "https://www.figma.com/file/ZLn1dvei35RylzFtbQbBp3/Confam-Money-Project?type=design&node-id=1-7",
+    livePreview: "https://www.confammoney.com/",
   },
   {
-    name: "Nba Clone",
+    name: "Finswich",
     description:
-      "This is a Mobile only Nba clone that gives users access to Nba team news, Games Highlights, Team Data, Nba standing.",
-    stack: ["SASS", "JavaScript", "React"],
-    sourceCode: "https://github.com/jidoggs/NBA-clone",
-    livePreview: "https://nba-clone.vercel.app/",
-  },
-  {
-    name: "Box Office Mojo Clone",
-    description:
-      "A static clone of the popular box office mojo landing page built using 90% HTMl tables.",
-    stack: ["CSS", "HTML"],
-    sourceCode: "https://github.com/jidoggs/box-mojo",
-    livePreview: "https://jidoggs.github.io/box-mojo/",
-  },
-  {
-    name: "Hacker News",
-    description: "A static clone of the popular Hacker news landing page.",
-    stack: ["CSS", "HTML"],
-    sourceCode: "https://github.com/jidoggs/hacker-news",
-    livePreview: "https://jidoggs.github.io/hacker-news/",
+      "Wallet interoperability for crypto platforms as well, further expanding the reach and convenience for users",
+    stack: ["Tailwind", "TypeScript", "React"],
+    livePreview: "https://finswich.fuspay.finance/",
   },
   {
     name: "Trafico",
@@ -63,7 +50,26 @@ const projects = [
       "A multi section landing page for a traffic management conultancy company.",
     stack: ["CSS", "JavaScript", "HTML"],
     sourceCode: "https://github.com/jidoggs/trafigo",
+    designLink:
+      "https://www.figma.com/file/Mibgc2ApJoTft9K41MVn9f/traffico-landing-page-for-figma?type=design&t=bMFq10irql9lFuCW-1",
     livePreview: "https://jidoggs.github.io/trafigo/",
+  },
+  {
+    name: "Box Office Mojo Clone",
+    description:
+      "A static clone of the popular box office mojo landing page built using 90% HTMl tables.",
+    stack: ["CSS", "HTML"],
+    sourceCode: "https://github.com/jidoggs/box-mojo",
+    designLink: "https://www.boxofficemojo.com/",
+    livePreview: "https://jidoggs.github.io/box-mojo/",
+  },
+  {
+    name: "Hacker News Clone",
+    description: "A static clone of the popular Hacker news landing page.",
+    stack: ["CSS", "HTML"],
+    sourceCode: "https://github.com/jidoggs/hacker-news",
+    designLink: "https://news.ycombinator.com/",
+    livePreview: "https://jidoggs.github.io/hacker-news/",
   },
 ];
 
