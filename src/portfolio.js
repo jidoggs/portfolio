091d4c9bbdabad0b249bab87a0528ed53c904c78
Olajide Shonuga <65728252+jidoggs@users.noreply.github.com>
2025-01-05 07:00:34 +0100

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -44,7 +44,8 @@ const projects = [
     description:
       "Nigerian government grant and loan scheme. Application had a 221,430 application per day at its peak traffic",
     stack: ["Antd", "Tailwind", "TypeScript", "Next"],
-    livePreview: "https://grant.fedgrantandloan.gov.ng/",
+    // livePreview: "https://grant.fedgrantandloan.gov.ng/",
+    livePreview: "https://new-grant-jidoggs-projects.vercel.app/",
   },
   {
     name: "Enzzyme",
@@ -69,23 +70,23 @@ const projects = [
       "https://www.figma.com/file/Mibgc2ApJoTft9K41MVn9f/traffico-landing-page-for-figma?type=design&t=bMFq10irql9lFuCW-1",
     livePreview: "https://jidoggs.github.io/trafigo/",
   },
-  {
-    name: "Box Office Mojo Clone",
-    description:
-      "A static clone of the popular box office mojo landing page built using 90% HTMl tables.",
-    stack: ["CSS", "HTML"],
-    sourceCode: "https://github.com/jidoggs/box-mojo",
-    designLink: "https://www.boxofficemojo.com/",
-    livePreview: "https://jidoggs.github.io/box-mojo/",
-  },
-  {
-    name: "Hacker News Clone",
-    description: "A static clone of the popular Hacker news landing page.",
-    stack: ["CSS", "HTML"],
-    sourceCode: "https://github.com/jidoggs/hacker-news",
-    designLink: "https://news.ycombinator.com/",
-    livePreview: "https://jidoggs.github.io/hacker-news/",
-  },
+  // {
+  //   name: "Box Office Mojo Clone",
+  //   description:
+  //     "A static clone of the popular box office mojo landing page built using 90% HTMl tables.",
+  //   stack: ["CSS", "HTML"],
+  //   sourceCode: "https://github.com/jidoggs/box-mojo",
+  //   designLink: "https://www.boxofficemojo.com/",
+  //   livePreview: "https://jidoggs.github.io/box-mojo/",
+  // },
+  // {
+  //   name: "Hacker News Clone",
+  //   description: "A static clone of the popular Hacker news landing page.",
+  //   stack: ["CSS", "HTML"],
+  //   sourceCode: "https://github.com/jidoggs/hacker-news",
+  //   designLink: "https://news.ycombinator.com/",
+  //   livePreview: "https://jidoggs.github.io/hacker-news/",
+  // },
 ];
 
 const skills = [
